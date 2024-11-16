57a4475b55e0a85ecb9f97aaf90ee197dd6c5b66
Tomo <ktomotomo58sky@gmail.com>
2024-11-16 14:46:52 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_課題_007</title>
+</head>
+<body>
+    <p>
+        <?php
+        $object_name = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($object_name);
+        ?>
+    </p>
+</body>
+</html>
